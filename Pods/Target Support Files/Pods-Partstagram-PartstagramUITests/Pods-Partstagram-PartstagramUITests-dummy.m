c19d27b05e95135af0db787fb7a9f65f00a4ba6b
Rohan Purandare <70453871+rohanpurandare@users.noreply.github.com>
2022-02-21 17:20:18 -0500

    Add files via upload

---
+++ Pods/Target Support Files/Pods-Partstagram-PartstagramUITests/Pods-Partstagram-PartstagramUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Partstagram_PartstagramUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Partstagram_PartstagramUITests
+@end
---
+++ Pods/Target Support Files/Pods-PartstagramTests/Pods-PartstagramTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PartstagramTests : NSObject
+@end
+@implementation PodsDummy_Pods_PartstagramTests
+@end
